65582cf6df379cac0e60de100ac47ae680e32a7e
Ezequiel Teruel <94491753+ezequieltc@users.noreply.github.com>
2024-08-29 17:43:52 -0300

    Se elimina el ejercicio3

--- src/Ejercicio3/IEdificio.java
+++
@@ -1,5 +0,0 @@
-package Ejercicio3;
-
-public interface IEdificio {
-	public double getSuperficieEdificio();
-}
--- src/Ejercicio3/IInstalacionDeportiva.java
+++
@@ -1,5 +0,0 @@
-package Ejercicio3;
-
-public interface IInstalacionDeportiva {
-	public String getTipoDeInstalacion();
-}
